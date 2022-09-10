ce54ea6a3dab103535ef1900105c628479bbca15
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-09-10 07:21:15 -0300

    att

--- limara/limaraAPI.js
+++ limara/limaraAPI.js
@@ -1,7 +1,7 @@
-import cors from "cors";
-import { create, router as _router } from "json-server";
-const server = create();
-const router = _router("limara.json");
+const cors = require("cors");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("limara.json");
 
 server.use(cors({ credentials: true, origin: "http://localhost:3002" }));
 
