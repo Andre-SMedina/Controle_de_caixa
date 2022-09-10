50eaa7ba6d8ce9be6d4ce6b1441913cdab16bdad
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-09-10 10:29:03 -0300

    att

--- app/src/components/Functions.js
+++ app/src/components/Functions.js
@@ -1,8 +1,9 @@
 function DataBase(obj, method, params = "", table) {
-  const op = 1;
+  const op = 0;
   const port = 3001;
 
   const server = op ? "localhost" : "54.209.185.105";
+  console.log(server);
   if (method === "GET") {
     const prod = fetch(`http://${server}:${port}/${table}${params}`, {
       method: method,
