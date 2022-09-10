32f1c5e250893697fc6fd26bb3700ce36b7f928d
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-09-10 08:29:42 -0300

    att

--- limara/limaraAPI.js
+++ limara/limaraAPI.js
@@ -3,7 +3,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("limara.json");
 
-server.use(cors({ credentials: true, origin: "http://localhost:3002" }));
+server.use(cors({ credentials: true, origin: "http://54.209.185.105:3002" }));
 
 server.use(router);
 server.listen(3001, () => {
