96d5d3f46c1028dc5af29f4ba9eb831ef6ab134d
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-09-09 21:20:51 -0300

    cors

--- app/outros/taianir/taianirAPI.js
+++ app/outros/taianir/taianirAPI.js
@@ -3,7 +3,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("taianir.json");
 
-server.use(cors({ credentials: true, origin: "http://localhost:3052" }));
+server.use(cors({ credentials: true, origin: "http://54.209.185.105:3052" }));
 
 server.use(router);
 server.listen(3051, () => {
